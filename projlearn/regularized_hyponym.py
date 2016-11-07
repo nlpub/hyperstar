af91103d22ec8fc68efcb356e87926296d0968e7
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2016-11-07 05:32:07 +0500

    Fix the dimensions

--- projlearn/regularized_hyponym.py
+++ projlearn/regularized_hyponym.py
@@ -11,10 +11,9 @@ class RegularizedHyponym(Baseline):
 
         self.lambda_       = kwargs['lambda_']
 
-        self.YX_hat        = tf.concat(1, [tf.ones((tf.shape(self.Y_hat)[0], 1)), self.Y_hat])
-        self.YY_hat        = tf.matmul(self.YX_hat, self.W)
+        self.YY_hat        = tf.matmul(self.Y_hat, self.W)
 
-        self.YY_similarity = tf.diag_part(tf.matmul(self.X[:, 1:], self.YY_hat, transpose_b=True))
+        self.YY_similarity = tf.diag_part(tf.matmul(self.X, self.YY_hat, transpose_b=True))
         self.YY_loss       = tf.nn.l2_loss(self.YY_similarity)
 
         self.loss          = tf.add(self.Y_loss, self.lambda_ * self.YY_loss)
--- projlearn/regularized_synonym.py
+++ projlearn/regularized_synonym.py
@@ -9,7 +9,7 @@ class RegularizedSynonym(RegularizedHyponym):
     def __init__(self, x_size, y_size, **kwargs):
         super().__init__(x_size, y_size, **kwargs)
 
-        self.YY_similarity = tf.diag_part(tf.matmul(self.Z[:, 1:], self.YY_hat, transpose_b=True))
+        self.YY_similarity = tf.diag_part(tf.matmul(self.Z, self.YY_hat, transpose_b=True))
         self.YY_loss       = tf.nn.l2_loss(self.YY_similarity)
 
         self.loss          = tf.add(self.Y_loss, self.lambda_ * self.YY_loss)
