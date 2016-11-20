bf073ad5997a5e4848c4ed90626354f8b0444992
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2016-11-20 21:05:47 +0500

    OK, now add it

---
+++ projlearn/frobenius_loss.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from .baseline import Baseline
+
+class FrobeniusLoss(Baseline):
+    """
+    Using the Frobenius norm as the loss function for the baseline approach.
+    """
+    def __init__(self, x_size, y_size, w_stddev, **kwargs):
+        super().__init__(x_size, y_size, w_stddev, **kwargs)
+
+        self.F_norm = tf.sqrt(tf.trace(tf.matmul(self.Y_error, tf.transpose(self.Y_error))))
+
+        self.loss   = self.F_norm
