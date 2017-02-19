a323d82985b4bc270041af68fb06df48af2b92ac
nvanva <ezhick179@gmail.com>
2017-02-19 21:33:50 +0000

    toyota fixes

--- projlearn/toyota.py
+++ projlearn/toyota.py
@@ -1,22 +1,25 @@
 import tensorflow as tf
+from conditional import conditional
 
 class Toyota:
     """
     A simple baseline that estimates the projection matrix W
     given the vectors X and Y without any constraints.
     """
-    def __init__(self, embs_type, embs_shape, w_stddev, **kwargs):
+    def __init__(self, embs_type, embs_shape, cpuembs, w_stddev, **kwargs):
         embs_dim = embs_shape[1]
-
-        self.init_embs_subgraph(embs_type, embs_shape)
-
-        # Inputs and vars
-        x_vec_ph = tf.placeholder(embs_type, [None, embs_dim], name='x_vec_ph')
-        y_ind_ph = tf.placeholder(tf.int32, [None, 1], name='y_ind_ph')
-
-        x = x_vec_ph  # n x dim #TODO: add x_ind placeholder and use EmbeddingsLookup(y_ind) instead of placeholder?
-        y_ind = y_ind_ph[:,0]  # n
-        y = tf.nn.embedding_lookup(self.embs_var, y_ind, name='y_embs_lookup')
+        with conditional(cpuembs, tf.device('/cpu:0')):
+            self.init_embs_subgraph(embs_type, embs_shape)
+       
+        # Inputs and vars 
+        with conditional(cpuembs, tf.device('/cpu:0')):
+            x_vec_ph = tf.placeholder(embs_type, [None, embs_dim], name='x_vec_ph')
+            y_ind_ph = tf.placeholder(tf.int32, [None, 1], name='y_ind_ph')
+            x = x_vec_ph  # n x dim #TODO: add x_ind placeholder and use EmbeddingsLookup(y_ind) instead of placeholder?
+            y_ind = y_ind_ph[:,0]  # n
+            y = tf.nn.embedding_lookup(self.embs_var, y_ind, name='y_embs_lookup')
+#            print(self.embs_var.get_shape(), self.embs_var.dtype)
+            print(y.get_shape(), y.dtype)
 
         # Predict hypernym vector from hyponym vector
         with tf.name_scope('xW'):
@@ -26,10 +29,10 @@ class Toyota:
         # Dot similarity of predicted hypernym with true hypernym
         with tf.name_scope('score_yhat_y'):
             pos_logit = tf.reduce_sum(y_hat * y, axis=1)  # n
-            print(pos_logit.get_shape())
+            print(pos_logit.get_shape(), pos_logit.dtype)
 
         # Dot similarity of predicted hypernym with all words
-        with tf.name_scope('score_yhat_embs'):
+        with tf.name_scope('score_yhat_embs'), conditional(cpuembs, tf.device('/cpu:0')):
             dot = tf.matmul(y_hat, self.embs_var, transpose_b=True)  # n x V
 
         with tf.name_scope('negative_ind'):
@@ -40,7 +43,13 @@ class Toyota:
             top_mask = tf.not_equal(y_ind, top_k_inds[:, 0], name='top_mask')  # n
             print(top_mask.get_shape())
             neg_ind = tf.where(top_mask, top_k_inds[:, 0], top_k_inds[:, 1])  # n
-            neg_logit = tf.where(top_mask, top_k_vals[:, 0], top_k_vals[:, 1])  # n
+#            neg_logit = tf.where(top_mask, top_k_vals[:, 0], top_k_vals[:, 1])  # n
+#            print(neg_logit.get_shape())
+
+        with tf.name_scope('score_yhat_yneg'):
+            with conditional(cpuembs, tf.device('/cpu:0')):
+                y_neg = tf.nn.embedding_lookup(self.embs_var, neg_ind, name='y_neg_embs_lookup')
+            neg_logit = tf.reduce_sum(y_hat * tf.stop_gradient(y_neg), axis=1) # n
             print(neg_logit.get_shape())
 
         # Loss
@@ -85,9 +94,9 @@ class Toyota:
             embs_ph = tf.placeholder(embs_type, embs_shape, name='embs_ph')
             embs_var = tf.get_variable('embs_var', shape=embs_shape, dtype=embs_type, trainable=False)
             embs_assign = tf.assign(embs_var, embs_ph, name='assign_embs_var')
-        self.embs_ph, self.embs_var, self.embs_assign = embs_ph, embs_var, embs_assign
+            self.embs_ph, self.embs_var, self.embs_assign = embs_ph, embs_var, embs_assign
 
-
+ 
     def __str__(self):
         return '<%s>' % self.__class__.__name__
 
