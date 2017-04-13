3697650bf70f610fd85bbd2397cc6533de33b8df
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-04-13 18:02:12 +0500

    Use grouping and HDF5

--- predict.py
+++ predict.py
@@ -4,18 +4,27 @@ import argparse
 import os
 import pickle
 from gensim.models.word2vec import Word2Vec
-import csv
 import sys
+import csv
+from itertools import zip_longest
 import numpy as np
 import tensorflow as tf
 from projlearn import *
+import h5py
+
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+
+def grouper(n, iterable, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fillvalue, *args)
 
 parser = argparse.ArgumentParser(description='Prediction.')
 parser.add_argument('--w2v',    required=True, type=argparse.FileType('rb'))
 parser.add_argument('--kmeans', default='kmeans.pickle', nargs='?', help='Path to k-means.pickle.')
 parser.add_argument('--model',  default='baseline', nargs='?', choices=MODELS.keys(), help='The model.')
 parser.add_argument('--path',   default='', nargs='?', help='The path to the model dump.')
-parser.add_argument('output',   type=argparse.FileType('wb'), help='The output npz file.')
+parser.add_argument('--slices', default=100000, type=int, help='The slice size.')
+parser.add_argument('hdf5')
 args = parser.parse_args()
 
 w2v = Word2Vec.load_word2vec_format(args.w2v, binary=True, unicode_errors='ignore')
@@ -27,38 +36,54 @@ kmeans = pickle.load(open(args.kmeans, 'rb'))
 
 print('The number of clusters is %d.' % kmeans.n_clusters, flush=True, file=sys.stderr)
 
-reader = csv.reader(sys.stdin, delimiter='\t', quoting=csv.QUOTE_NONE)
-
-X_all, Y_all = [], []
-
-for row in reader:
-    X_all.append(w2v.wv.vocab[row[0]].index)
-    Y_all.append(w2v.wv.vocab[row[1]].index)
-
-X_all, Y_all = w2v.wv.syn0[X_all], w2v.wv.syn0[Y_all]
-
-offsets = Y_all - X_all
-
-X_clusters_list = list(enumerate(kmeans.predict(offsets)))
-
-X_clusters = {}
-
-for cluster in range(kmeans.n_clusters):
-    X_clusters[cluster] = [i for i, c in X_clusters_list if c == cluster]
-
 model = MODELS[args.model](x_size=w2v.layer1_size, y_size=w2v.layer1_size, w_stddev=0, lambda_=0)
 
-Y_hat_all = np.empty(X_all.shape)
+reader = csv.reader(sys.stdin, delimiter='\t', quoting=csv.QUOTE_NONE)
 
-for cluster, indices in X_clusters.items():
-    with tf.Session() as sess:
-        saver = tf.train.Saver()
+try:
+    os.remove(args.hdf5)
+except OSError:
+    pass
 
-        saver.restore(sess, os.path.join(args.path, '%s.k%d.trained') % (args.model, cluster + 1))
+with h5py.File(args.hdf5, 'w') as f:
+    dset = f.create_dataset('hyperstar', (0, w2v.layer1_size), maxshape=(None, w2v.layer1_size), chunks=True)
 
-        Y_hat = sess.run(model.Y_hat, feed_dict={model.X: X_all[indices]})
+    for rows in grouper(args.slices, reader):
+        X_all, Y_all = [], []
 
-        for i, j in enumerate(indices):
-            Y_hat_all[j] = Y_hat[i]
+        for row in rows:
+            if row is None:
+                continue
 
-np.savez_compressed(args.output, Y_hat_all=Y_hat_all)
+            X_all.append(w2v.wv.vocab[row[0]].index)
+            Y_all.append(w2v.wv.vocab[row[1]].index)
+
+        X_all, Y_all = w2v.wv.syn0[X_all], w2v.wv.syn0[Y_all]
+
+        offsets = Y_all - X_all
+
+        X_clusters_list = list(enumerate(kmeans.predict(offsets)))
+
+        X_clusters = {}
+
+        for cluster in range(kmeans.n_clusters):
+            X_clusters[cluster] = [i for i, c in X_clusters_list if c == cluster]
+
+        Y_hat_all = np.empty(X_all.shape)
+
+        for cluster, indices in X_clusters.items():
+            with tf.Session() as sess:
+                saver = tf.train.Saver()
+
+                saver.restore(sess, os.path.join(args.path, '%s.k%d.trained') % (args.model, cluster + 1))
+
+                Y_hat = sess.run(model.Y_hat, feed_dict={model.X: X_all[indices]})
+
+                for i, j in enumerate(indices):
+                    Y_hat_all[j] = Y_hat[i]
+
+        dset.resize(dset.shape[0] + Y_hat_all.shape[0], axis=0)
+
+        dset[-Y_hat_all.shape[0]:] = Y_hat_all
+
+        print('The number of rows is %d.' % dset.shape[0], flush=True, file=sys.stderr)
